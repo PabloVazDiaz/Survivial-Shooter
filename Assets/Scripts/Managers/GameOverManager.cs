2d5a7574d29c255809a0fd2b118a03b8b3288385
Anonimo-PC\Anonimo <pablo.vazquez@alumnos.fesac.es>
2018-11-27 13:05:33 +0100

    Upload finished project to GitHub

---
+++ Assets/Scripts/Managers/GameOverManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class GameOverManager : MonoBehaviour
+{
+    public PlayerHealth playerHealth;
+    public float restarDelay = 5f;
+
+
+    Animator anim;
+    float restartTimer;
+
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+
+    void Update()
+    {
+        if (playerHealth.currentHealth <= 0)
+        {
+            anim.SetTrigger("GameOver");
+
+            restartTimer += Time.deltaTime;
+
+            if (restartTimer >= restarDelay){
+                Application.LoadLevel(Application.loadedLevel);
+            }
+        }
+    }
+}
